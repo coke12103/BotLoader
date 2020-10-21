c79daa5b24fe496f37755bc82e84dae3b5049096
coke12103 <coke12103@gmail.com>
2020-10-21 20:56:11 +0900

    add about plugin

---
+++ plugins/about/index.js
@@ -0,0 +1,22 @@
+const App = require('../../');
+const AppInfo = require('../../package.json');
+
+class About extends App.PluginBase{
+  constructor(){
+    super('About Plugin');
+    super.addCommand('about');
+
+    try{
+      this.more_info = require("../../more_info.json").texts;
+    }catch(e){
+      this.more_info = [];
+      console.log(e);
+    }
+  }
+
+  onCommand(command, argv, message){
+    message.channel.send(`**${AppInfo.name} ${AppInfo.version}**\n${App.PluginCount}個のプラグインが読み込まれています。\n\n${this.more_info.join("\n")}`);
+  }
+}
+
+module.exports = About;
