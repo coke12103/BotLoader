f11b701e10f5b9f81afe5829364529f66c2538b7
coke12103 <coke12103@gmail.com>
2020-10-21 11:57:04 +0900

    fix

--- src/loader.js
+++ src/loader.js
@@ -1,15 +1,26 @@
 const fs = require('fs');
+const path = require('path');
+
+const PL_DIR = './plugins/';
 
 class Loader{
   static load(){
     var plugins = [];
 
     try{
-      var file_list = fs.readdirSync('./plugins/');
+      var file_list = fs.readdirSync(PL_DIR);
 
       for(var file of file_list) {
-        var pl = require('../plugins/' + file);
-        plugins.push(new pl());
+        try{
+          var index = file;
+
+          if(fs.statSync(path.join(PL_DIR, index)).isDirectory()) index = path.join(index, "index.js");
+
+          var pl = require(path.relative(__dirname, path.join(PL_DIR, index)));
+          plugins.push(new pl());
+        }catch(e){
+          console.log(e);
+        }
       }
     }catch(e){
       console.log(e);
