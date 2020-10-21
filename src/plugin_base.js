922c437b1241e45cec918d4182fc820d9d9887db
coke12103 <coke12103@gmail.com>
2020-10-21 10:06:23 +0900

    add onCommand

--- src/plugin_base.js
+++ src/plugin_base.js
@@ -2,10 +2,19 @@ class PluginBase{
   constructor(name = 'New Plugin', description = "none"){
     this.name = name;
     this.description = description;
+    this.commands = [];
   }
 
   onMessage(message){
     return;
+  }
+
+  onCommand(command, argv, message){
+    return;
+  }
+
+  addCommand(command, description = "none"){
+    this.commands.push({command: command, description: description});
   }
 }
 
